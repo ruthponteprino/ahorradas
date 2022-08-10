124b0e1d976aca37ff1f06c05addab5448468fb1
AnyiGP <angelesgp31@hotmail.com>
2022-08-10 00:05:26 -0300

    agrego btn agregar categorias

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const btnAhorradas = document.getElementById('btn-ahorradas')
 const btnCategorias = document.getElementById('btnCategorias')
 const btnReportes = document.getElementById('btnReportes')
 const btnNuevaOperacion = document.getElementById('btn-nueva-operacion')
-const btnOcultarFiltros = document.getElementById('btn-ocultar-filtros') 
-const btnMostrarFiltros = document.getElementById('btn-mostrar-filtros') 
+const btnOcultarFiltros = document.getElementById('btn-ocultar-filtros')
+const btnMostrarFiltros = document.getElementById('btn-mostrar-filtros')
 const btnAgregar = document.getElementById('agregar-btn')
 const editarOperacion = document.getElementById('editarOperacion')
 const eliminarOperacion = document.getElementById('eliminarOperacion')
+const btnAgregarCategoria = document.getElementById('btn-agregar-categoria')
 
 
 // INPUTS //
@@ -22,7 +23,7 @@ const acciones = document.getElementById('acciones')
 //SECCIONES//
 const balance = document.getElementById('seccion-balance') //TRAIGO SECCION BALANCE
 const categorias = document.getElementById('seccion-categorias') //TRAIGO SECCION CATEGORIAS
-const reportes = document.getElementById ('seccion-reportes') //TRAIGO SECCION REPORTES
+const reportes = document.getElementById('seccion-reportes') //TRAIGO SECCION REPORTES
 const seccionOperacion = document.getElementById('seccion-operacion') //TRAIGO FORMULARIO OPERACIONES
 const filtros = document.getElementById('filtros')
 
@@ -55,27 +56,27 @@ btnReportes.addEventListener('click', () => {
     reportes.classList.remove('oculto')
 })
 
- btnNuevaOperacion.addEventListener('click', () => {
+btnNuevaOperacion.addEventListener('click', () => {
     seccionOperacion.classList.remove('oculto')
     balance.classList.add('oculto')
     categorias.classList.add('oculto')
-     reportes.classList.add('oculto')
- })
+    reportes.classList.add('oculto')
+})
 
- //FILTROS//
+//FILTROS//
 //OCULTAR Y MOSTRAR FILTROS
 
 btnOcultarFiltros.addEventListener('click', () => {
     filtros.classList.add('oculto')
     btnMostrarFiltros.classList.remove('oculto')
     btnOcultarFiltros.classList.add('oculto')
-} )
+})
 
 btnMostrarFiltros.addEventListener('click', () => {
     filtros.classList.remove('oculto')
     btnMostrarFiltros.classList.add('oculto')
     btnOcultarFiltros.classList.remove('oculto')
-} )
+})
 
 // CATEGORIAS //
 
@@ -83,49 +84,79 @@ const arrayCategorias = ['Comida', 'Servicios', 'Salidas', 'Educacion', 'Transpo
 
 const generarCategorias = () => {
     const selects = document.getElementsByClassName('select-categorias')
-    for(let i = 0; i < selects.length; i++){
+    for (let i = 0; i < selects.length; i++) {
         const select = selects[i]
-        if(select.classList.contains('filtro-categorias')){
+        if (select.classList.contains('filtro-categorias')) {
             select.innerHTML = '<option value="todas" selected>Todas</option>'
         }
         arrayCategorias.forEach(categoria => {
-             select.innerHTML += `<option value=${categoria}>${categoria}</option>`
+            select.innerHTML += `<option value=${categoria}>${categoria}</option>`
         })
     }
 }
 generarCategorias()
 
+//
+//MOSTRAR CATEGORIAS EXISTENTES EN SECCION CATEGORIAS
+// 
+
+//
+//RECORRER CATEGORIAS Y AGREGAR lo que el usuario escriba en el input
+//
+//1-traer el input
+//2-agregar/pintar en la tabla de categorias lo que usuario escriba en la tabla
+
+const inputCategoria = document.getElementById('nueva-categoria-input')
+const agregarCategoria = () => {
+    arrayCategorias.push(inputCategoria.value)
+    // inputCategoria.innerHTML = `<p>${categorias}</p>`
+    console.log(arrayCategorias)
+} 
+
+const limpiarInputAgregarCategorias = () => {
+    inputCategoria.value = ''
+}
+
+btnAgregarCategoria.addEventListener('click', () => {
+    agregarCategoria()
+    limpiarInputAgregarCategorias()
+})
+
+//imprimir la nueva categoria en el div de abajo con las otras categorias y el los select que aparezcan las nuevas
+
+const pintarNuevaCategoria = () => {
+    select.innerHTML += `<option value=${arrayCategorias}>${arrayCategorias}</option>` //no me estaria saliendo
+}
+
 // NUEVA OPERACION //
 
-const operaciones = [ //objetos para prueba de filtros luego borrar
-]
+const operaciones = []
 
 const mostrarOperaciones = (arr) => {
-    if(!arr.length){
+    if (!arr.length) {
         document.getElementById('con-operaciones').classList.add('d-none')
     } else {
         document.getElementById('con-operaciones').classList.remove('d-none')
         document.getElementById('sin-operaciones').classList.add('d-none')
     }
 }
-
 mostrarOperaciones(operaciones)
 
-btnAgregar.addEventListener('click', ()=> {
+btnAgregar.addEventListener('click', () => {
     const operacion = {
         //id : uuidv4(), NO FUNCIONA!
-        descripcion : inputDescripcion.value,
-        monto : inputMonto.value,
-        tipo : inputTipo.value,
-        categorias : selectCategoriaOperacion.value,
-        fecha : inputFecha.value,
-        acciones : acciones.value
+        descripcion: inputDescripcion.value,
+        monto: inputMonto.value,
+        tipo: inputTipo.value,
+        categorias: selectCategoriaOperacion.value,
+        fecha: inputFecha.value,
+        acciones: acciones.value
     }
 
     seccionOperacion.classList.add('oculto')
     balance.classList.remove('oculto')
     operaciones.push(operacion)
-    
+
     inputDescripcion.value = ""
     inputMonto.value = 0
     inputTipo.value = "gasto"
@@ -134,18 +165,20 @@ btnAgregar.addEventListener('click', ()=> {
 
     mostrarOperaciones(operaciones)
     pintarOperaciones(operaciones)
-})        
+})
 
 const pintarOperaciones = arr => {
     let str = ''
     arr.forEach((operacion) => {
         str = str + `
-        <table class="table table-borderless"><tr>
-                  <td>${operacion.descripcion}</td>
-                   <td>${operacion.categoria}</td>
-                   <td>${operacion.fecha}</td>
-                   <td>${operacion.monto}</td>
-                </tr></table>`
+        <table class="table table-borderless">
+            <tr>
+                <td>${operacion.descripcion}</td>
+                <td>${operacion.categoria}</td>
+                <td>${operacion.fecha}</td>
+                <td>${operacion.monto}</td>
+            </tr>
+        </table>`
     })
     document.getElementById('operaciones').innerHTML = str
 }
@@ -177,53 +210,61 @@ selectTipo.addEventListener('change', (e) => {
     const tipoTodas = [...operaciones]
     const tipoGastos = []
     const tipoGanacias = []
-    })
-    
-    //tipo
+})
 
-    const selectTipoFiltros = document.getElementById('selectTipoFiltros')
+//tipo
 
-    selectTipoFiltros.addEventListener('change', (e) => {
-        console.log(e.target.value)
-        if(e.target.value !== 'todos') {
-            const porTipo = operaciones.filter(operaciones => operaciones.tipo === e.target.value)
-            console.log(porTipo)
-        } else {
-            console.log(operaciones)
-        }
-    })
+const selectTipoFiltros = document.getElementById('selectTipoFiltros')
 
-    //categorias
+selectTipoFiltros.addEventListener('change', (e) => {
+    console.log(e.target.value)
+    if (e.target.value !== 'todos') {
+        const porTipo = operaciones.filter(operaciones => operaciones.tipo === e.target.value)
+        console.log(porTipo)
+    } else {
+        console.log(operaciones)
+    }
+})
 
-    const selectCategoriaFiltros = document.getElementById('selectCategoriaFiltros')
+//categorias
 
-    selectCategoriaFiltros.addEventListener('change', (e) => {
-        console.log(e.target.value)
-        if(e.target.value !== 'todas') {
-            const porCategoria = operaciones.filter(operaciones => operaciones.categoria === e.target.value)
-            console.log(porCategoria)
-        } else {
-            console.log(operaciones)
-        }
+const selectCategoriaFiltros = document.getElementById('selectCategoriaFiltros')
+
+selectCategoriaFiltros.addEventListener('change', (e) => {
+    console.log(e.target.value)
+    if (e.target.value !== 'todas') {
+        const porCategoria = operaciones.filter(operaciones => operaciones.categoria === e.target.value)
+        console.log(porCategoria)
+    } else {
+        console.log(operaciones)
+    }
     //los values tiene que estar escritos igual
-    })
+})
 
-    //cómo hago para que los resultados de los filtros se muestren/impriman en operaciones, con un filter?
+//cómo hago para que los resultados de los filtros se muestren/impriman en operaciones, con un filter?
 
-    //terminar de hacer filtros para fecha y ordenar por
+//terminar de hacer filtros para fecha y ordenar por
 
-    //REPORTES
+//REPORTES
 
-    //mostrar contenedor-reportes ocultar por defecto, mostrar solo cuando hay operaciones
+//mostrar contenedor-reportes ocultar por defecto, mostrar solo cuando hay operaciones
 
-    const contenedorReportes = document.getElementById('contenedor-reportes')
+const contenedorReportes = document.getElementById('contenedor-reportes')
+const operacionesInsuficientes = document.getElementById('operaciones-insuficientes')
 
-    // contenedorReportes.addEventListener(
-    //     //si recibe operaciones muestra contenedor-reportes, si no muestra div de operaciones insuficientes.
-    //     //VER COMO SE HACE
-    // 1-recorrer operaciones y si hay al menos 1 operacion cargada
-    //2-si esto se cumple, entonces sacarle la clase oculto y mostrar contenedor-resumen
+//     //si recibe operaciones muestra contenedor-reportes, si no muestra div de operaciones insuficientes.
+//     //VER COMO SE HACE
+// 1-recorrer operaciones y si hay al menos 1 operacion cargada
+//2-si esto se cumple, entonces sacarle la clase oculto y mostrar contenedor-resumen
 
-    //     contenedorReportes.classList.remove('oculto')
-    // )
-    
+const mostrarReportes = (arr) => {
+    if (!arr.length) { //si el arr esta vacio hace esto...
+        contenedorReportes.classList.add('oculto') //'mostrar'
+    } else { //sino muestra esto
+        contenedorReportes.classList.add('mostrar') ////!!!funciona si le meto un dato al arr operaciones manualmente, pero no si lo agrego desde agregar operaciones, es como que no me detecta si hay o no operaciones
+        operacionesInsuficientes.classList.add('oculto')
+    }
+}
+mostrarReportes(operaciones)
+
+
